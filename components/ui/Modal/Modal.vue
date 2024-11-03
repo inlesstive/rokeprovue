98a1da3f8a6ad5979bf81bce7017333339f3c5ca
Karma-122 <darkkingrust@mail.ru>
2024-11-03 21:02:43 +0500

    52

---
+++ components/ui/Modal/Modal.vue
@@ -0,0 +1,91 @@
+<template>
+    <section class="modal">
+        <div class="overlay"></div>
+        <div class="modal__popup">
+            <div class="modal__popup-container">
+                    <div class="modal__popup-box">
+                                <button class="modal__popup-button--closer">
+                                    <svg xmlns="http://www.w3.org/2000/svg" width="25" height="25" viewBox="0 0 25 25" fill="none">
+                                        <path d="M0.237178 0.489985L0.354307 0.354307C0.783775 -0.075161 1.45581 -0.114193 1.92937 0.237178L2.06505 0.354307L12.5 10.7887L22.935 0.354307C23.4074 -0.118096 24.1732 -0.118096 24.6457 0.354307C25.1181 0.826711 25.1181 1.59265 24.6457 2.06505L14.2113 12.5L24.6457 22.935C25.0752 23.3644 25.1142 24.0365 24.7628 24.51L24.6457 24.6457C24.2163 25.0752 23.5442 25.1142 23.0707 24.7628L22.935 24.6457L12.5 14.2113L2.06505 24.6457C1.59265 25.1181 0.826711 25.1181 0.354307 24.6457C-0.118096 24.1732 -0.118096 23.4074 0.354307 22.935L10.7887 12.5L0.354307 2.06505C-0.075161 1.63558 -0.114193 0.96355 0.237178 0.489985Z" fill="#212121"/>
+                                    </svg>
+                                </button>
+
+                            <div class="modal__popup-inputs">
+                                <input type="text" class="modal__popup-input" placeholder="Название реквизита*">
+                                <input type="text" class="modal__popup-input" placeholder="Полное фирменное наименование**">
+                                <input type="text" class="modal__popup-input" placeholder="Электронная почта*">
+                                <input type="text" class="modal__popup-input" placeholder="ИНН*">
+                                <input type="text" class="modal__popup-input" placeholder="КПП*">
+                                <input type="text" class="modal__popup-input" placeholder="ОГРН*">
+                                <input type="text" class="modal__popup-input" placeholder="ОКПО*">
+                                <input type="text" class="modal__popup-input" placeholder="ОКАТО*">
+                                <input type="text" class="modal__popup-input" placeholder="ОКТМО*">
+                                <input type="text" class="modal__popup-input" placeholder="Юридический адрес*">
+                                <input type="text" class="modal__popup-input" placeholder="Почтовый адрес*">
+                                <input type="text" class="modal__popup-input" placeholder="БИК*">
+                                <input type="text" class="modal__popup-input" placeholder="Телефон*">
+
+                            </div>
+                            <UiButtonSendButton>Сохранить</UiButtonSendButton>
+                    </div> 
+            </div>
+        </div>
+    </section>  
+</template>
+
+<style lang="scss">
+.overlay{
+    position: fixed;
+    width: 100vw;
+    height: 100vh;
+    top: 0;
+    left: 0;
+    background: black;
+    opacity: 0.6;
+}
+
+.modal{
+
+    &__popup{
+    position: absolute;
+    z-index: 100;
+    top: 100px;
+    left: 435px;
+    width: 1050px;
+    height: 100vh;
+    padding: 30px;
+    background: white
+    }
+
+    &__popup-container{
+        display: flex;
+        flex-direction: column;
+    }
+    &__popup-inputs{
+        display: grid;
+        grid-template-columns:repeat(2, minmax(0, 1fr)) ;
+        grid-gap: 30px;
+        margin-bottom: 20px;
+    }
+    &__popup-input{
+        color: #808080;
+        font-family: "Myriad Pro";
+        font-size: 24px;
+        font-style: normal;
+        font-weight: 400;
+        line-height: 100%;
+        width: 100%;
+        height: 100%;
+        display: flex;
+        padding: 10px 0px;
+        outline: none;
+        border: none;
+        border-bottom: 1px solid #d9d9d9;
+        &:nth-child(1) {
+            grid-column: 1 / -1;
+        }
+    }
+}
+
+
+</style>
--- pages/account/profile.vue
+++ pages/account/profile.vue
@@ -70,10 +70,8 @@
           </div>
         </div>
 
-        <div
-          class="profile__section profile__section--password-change"
-          v-if="currentTab === 'profile'"
-        >
+        <div class="profile__section profile__section--password-change"
+          v-if="currentTab === 'profile'">
           <span class="profile__section-title">Смена пароля</span>
           <div class="profile__inputs-grid--full">
             <input
@@ -168,15 +166,15 @@
                 </thead>
                 <tbody class="favorites__table-body">
                     <tr class="favorites__table-row" v-for="_ in 3">
-                        <td class="favorites__table-row--delete">
+                        <td class="favorites__product-name">
                             <button class="favorites__delete">
                                 <svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" viewBox="0 0 20 20" fill="none">
                                     <path d="M0.189743 0.391988L0.283446 0.283446C0.62702 -0.0601288 1.16465 -0.0913545 1.5435 0.189743L1.65204 0.283446L10 8.63097L18.348 0.283446C18.7259 -0.0944772 19.3386 -0.0944772 19.7165 0.283446C20.0945 0.661369 20.0945 1.27412 19.7165 1.65204L11.369 10L19.7165 18.348C20.0601 18.6915 20.0914 19.2292 19.8102 19.608L19.7165 19.7165C19.373 20.0601 18.8354 20.0914 18.4565 19.8102L18.348 19.7165L10 11.369L1.65204 19.7165C1.27412 20.0945 0.661369 20.0945 0.283446 19.7165C-0.0944772 19.3386 -0.0944772 18.7259 0.283446 18.348L8.63097 10L0.283446 1.65204C-0.0601288 1.30847 -0.0913545 0.77084 0.189743 0.391988Z" fill="#212121"/>
                                 </svg>
                             </button>
-                        </td>
-                        <td class="favorites__product-name">
-                            Шаровый гидравлический кран
+                            <span class="favorites__product-name--span">
+                                Шаровый гидравлический кран
+                            </span>
                         </td>
                         <td class="favorites__product-description">
                             <ul class="favorites__description-list">
@@ -192,14 +190,27 @@
                         </td>
                         <td class="favorites__counter">
                             <div class="favorites__counter-controls">
-                                <button class="favorites__counter-button">-</button>
+                                <button class="favorites__counter-button">
+                                    <svg xmlns="http://www.w3.org/2000/svg" width="14" height="14" viewBox="0 0 14 14" fill="none">
+                                        <rect y="6" width="14" height="2" fill="black"/>
+                                    </svg>
+                                </button>
                                 <span class="favorites__counter-value">1</span>
-                                <button class="favorites__counter-button">+</button>
+                                <button class="favorites__counter-button">
+                                    <svg xmlns="http://www.w3.org/2000/svg" width="14" height="14" viewBox="0 0 14 14" fill="none">
+                                        <rect y="6" width="14" height="2" fill="black"/>
+                                        <rect x="6" y="14" width="14" height="2" transform="rotate(-90 6 14)" fill="black"/>
+                                    </svg>
+                                </button>
                             </div>
                         </td>
-                        <td class="favorites__sum">500 ₽</td>
-                        <td>
-                            <UiButtonSendButton class="favorites__button">Добавить в корзину</UiButtonSendButton>
+                        <td class="favorites__sum">
+                            <span>
+                                500 ₽
+                            </span>
+                        </td>
+                        <td class="favorites__buyer">
+                            <UiButtonSendButton class="favorites__byer-button">Добавить в корзину</UiButtonSendButton>
                         </td>
                     </tr>
                 </tbody>
@@ -208,8 +219,26 @@
 
 
         <div v-if="currentTab === 'details'" class="profile__section">
-          <span class="profile__section-title">Реквизиты</span>
-          <p>Здесь вы можете добавить или изменить реквизиты.</p>
+            <div class="details">
+                <ul class="details__list">
+                    <li class="details__list-item">
+                        <span class="details__message">Реквизиты не добавлены</span>
+                    </li>
+                    <li class="details__list-item">
+                        <p class="details__description">
+                            У вас пока нет сохраненных реквизитов. Добавьте <br> данные — это ускорит оформление заказа.
+                        </p>
+                    </li>
+                    <li class="details__list-item">                            
+                            <button class="details__popup-button"> 
+                                <span>
+                                    Добавить новые реквизиты
+                                </span>
+                            </button>
+                    </li>
+                </ul>
+            </div>
+            <UiModal></UiModal>
         </div>
       </div>
     </div>
@@ -241,13 +270,167 @@ const pageTitle = computed(() => {
 </script>
 
 <style lang="scss" scoped>
+.details{
+    &__list{
+        display: flex;
+        align-items: flex-start ;
+        gap: 20px;
+        flex-direction: column ;
+    }
+    display: flex;
+    justify-content: center;
+    flex-direction: column;
+    align-items: center;
+    &__message{
+        color: #000;
+        font-family: "Myriad Pro";
+        font-size: 40px;
+        font-style: normal;
+        font-weight: 400;
+        line-height: 90%; 
+    }
+    &__description{
+        color: #000;
+        font-family: "Myriad Pro";
+        font-size: 20px;
+        font-style: normal;
+        font-weight: 400;
+        line-height: 100%;
+    }
+    &__popup-button{
+        padding: 10px;
+        background-color: white;
+        color: #000;
+        text-transform: none;
+        border: 1px solid #D9D9D9;
+        cursor: pointer;
+        font-size: 20px ;
+        font-style: normal;
+        font-weight: 400;
+        line-height: 100%; 
+        font-family: "Myriad Pro";
+        transition: 0.3s ;
+    }
+    &__popup-button:hover{
+        background-color: #3064C7;
+        color: white;
+    }
+}
+
+.contacts__tab-button--active{
+    background: #3064C7;
+    color: white;
+}
+
 .favorites{
-    &__table-row{
+    &__byer-button{
+        max-width: 279px;
+    }
+    &__buyer{
+        padding: 10px;
+    }
+    &__sum span{
+        color: #000;
+        font-family: "Myriad Pro";
+        font-size: 24px;
+        font-style: normal;
+        font-weight: 400;
+        line-height: 100%; 
+        display: flex;
+        justify-content: center;
+        align-items: center;
+    }
+    &__sum{
+        padding: 10px;
+    }
+    &__counter-button{
+        cursor: pointer;
+        background-color: white;
+    }
+    &__counter-value{
+        margin-left: 20px;
+        margin-right: 20px;
+    }
+    &__counter-controls{
+        border: 1px solid #D9D9D9;
+        color: #000;
+        font-family: "Myriad Pro";
+        font-size: 20px;
+        font-style: normal;
+        font-weight: 600;
+        line-height: 90%;
+        display: flex;
+        justify-content: center;
+        padding: 11px 0px ;
+    }
+    &__counter{
+        padding: 10px;
+    }
+    &__description-list{
+        display: flex;
+        flex-direction: column;
+        gap: 10px;
+    }
+    &__description-item{
+        color: #000;
+        font-family: "Myriad Pro";
+        font-size: 18px;
+        font-style: normal;
+        font-weight: 400;
+        line-height: 90%;
+    }
+    &__description-label{
+        color: #000;
+        font-family: "Myriad Pro";
+        font-size: 18px;
+        font-style: normal;
+        font-weight: 400;
+        line-height: 90%; 
+        opacity: 0.4;
+    }
+    &__product-name--span{
+        width: 100%;
+        text-align: center;
+        color: #000;
+        font-family: "Myriad Pro";
+        font-size: 24px;
+        font-style: normal;
+        font-weight: 400;
+        line-height: 100%;
+        padding: 10px 20px;
+    }
+    &__buyer{
+        border: 1px solid #D9D9D9;
+    }
+    &__sum{
+        border: 1px solid #D9D9D9;
+    }
+    &__counter{
+        border: 1px solid #D9D9D9;
+    }
+    &__product-description{
+        padding: 10px 20px; 
+        border: 1px solid #D9D9D9;
+    }
+    &__product-name{
+        max-width: 520px;    
+        border: 1px solid #D9D9D9;
+    }
+    &__table-header-item{
+        color: #000;
+        font-family: "Myriad Pro";
+        font-size: 20px;
+        font-style: normal;
+        font-weight: 400;
+        line-height: 90%; 
+        padding: 17px;
         border: 1px solid #D9D9D9;
     }
     &__delete{
+        cursor: pointer;
+        background-color: white; 
         padding: 33px 21px;
-        border: 1px solid #D9D9D9;
+        border-right: 1px solid #D9D9D9;
     }
     &__table-body{
         border: 1px solid #D9D9D9;
@@ -259,7 +442,6 @@ const pageTitle = computed(() => {
     font-style: normal;
     font-weight: 400;
     line-height: 90%;
-    border: 1px solid #D9D9D9;
     }
 
   &__table{
@@ -272,9 +454,7 @@ const pageTitle = computed(() => {
 
 
 .profile {
-<<<<<<< HEAD
 
-=======
   &__section-table-wrapper {
     display: flex;
     flex-direction: column;
@@ -283,7 +463,7 @@ const pageTitle = computed(() => {
       margin-left: 20px;
     }
   }
->>>>>>> 39446b69ddd28208b20d2dfb8ba5d5421ca3edb3
+
   &__logout {
     color: #17172d;
     font-family: "Myriad Pro";
